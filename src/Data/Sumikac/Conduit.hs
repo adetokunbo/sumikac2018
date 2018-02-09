ab5cf1baeed7aa0db0baad88b400181e0ab0c2a2
Tim Emiola <adetokunbo@users.noreply.github.com>
2018-02-09 15:56:48 +0900

    Experiment with using LitDesc instead of LiteralDescription
    - The new Sum type results in fewer optional values and so allows for
      less effort from the parsing pipeline

--- src/Data/Sumikac/Conduit.hs
+++ src/Data/Sumikac/Conduit.hs
@@ -5,7 +5,6 @@ module Data.Sumikac.Conduit
   -- * Yaml Parsing functions
     convert1File
   , convertFilesInDir
-  , pipeProducts
   , pipeDecoded
   , pipeEitherDecoded
   , mkProductPipe
@@ -29,6 +28,7 @@ import qualified Data.Conduit.List            as CL
 import qualified Data.Conduit.Text            as CT
 import           Data.Monoid                  ((<>))
 import           Data.Text                    (Text)
+import qualified Data.Text                    as Text
 import           Data.Yaml                    (ParseException (..), decode,
                                                decodeEither')
 import           System.Directory             (createDirectoryIfMissing,
@@ -37,22 +37,48 @@ import           System.FilePath
 
 import           Data.Sumikac.Types
 
+-- | To generate from the combined data
+-- save the products
+-- save the en descriptions
+-- scan the product directory, report on files that do not have product description pair
+-- forward pairs use the combined data-type to the html generator pipeline
+
+-- | Creates a 'ConvertPipeline' for 'LitDesc'
+mkLitDescPipe
+  :: (MonadThrow m, MonadIO m)
+  => ConvertPipeline LitDesc o m
+mkLitDescPipe = ConvertPipeline
+  { cpParse = pipeLitDesc
+  , cpGo = summarize .| toStdout
+  , cpError = dumpParseException
+  }
+  where
+    showBoth (name, content) = Text.pack name <> "\n" <> content
+    summarize = CC.map summarizeLD .| CC.map showBoth
+    toStdout = CC.map Text.unpack .| CC.unlines .| CC.map pack .| CC.stdout
+
+-- | Specializes 'pipeEitherDecoded' for 'LitDesc'
+pipeLitDesc
+  :: (Monad m, MonadThrow m)
+  => ConduitM ByteString (Either ParseException LitDesc) m ()
+pipeLitDesc = pipeEitherDecoded
+
 -- | Creates a 'ConvertPipeline' for 'LiteralDescription'
 mkLiteralDescriptionPipe
   :: (MonadThrow m, MonadIO m)
-  => FilePath
+  => FilePath -- ^ the destination directory
   -> ConvertPipeline LiteralDescription o m
-mkLiteralDescriptionPipe dstDir = ConvertPipeline
-  { cpParse = decodedLiteralDescription
-  , cpGo = yieldFileAndProductName dstDir .| CC.map fst .| CC.unlines .| CC.map pack .| CC.stdout
+mkLiteralDescriptionPipe d = ConvertPipeline
+  { cpParse = pipeLiteralDescriptions
+  , cpGo = yieldFileAndProductName d .| CC.map fst .| CC.unlines .| CC.map pack .| CC.stdout
   , cpError = dumpParseException
   }
 
--- | Specializes 'pipeDecoded' to yield 'LiteralDescription'
-decodedLiteralDescription
+-- | Specializes 'pipeEitherDecoded' for 'LitDesc'
+pipeLiteralDescriptions
   :: (Monad m, MonadThrow m)
   => ConduitM ByteString (Either ParseException LiteralDescription) m ()
-decodedLiteralDescription = pipeEitherDecoded
+pipeLiteralDescriptions = pipeEitherDecoded
 
 -- | Yields the filename and Yaml-encoded output of a 'Product'
 yieldFileAndProductName
@@ -64,48 +90,31 @@ yieldFileAndProductName dstDir = CC.map $ fileAndProductName dstDir
 -- | Creates a 'ConvertPipeline' for 'Product'
 mkProductPipe
   :: (MonadThrow m, MonadIO m, MonadResource m)
-  => FilePath
+  => FilePath -- ^ the destination directory
   -> ConvertPipeline (ByteString, Product) o m
-mkProductPipe dstDir = ConvertPipeline
+mkProductPipe d = ConvertPipeline
   { cpParse = pipeProductsKeep
-  , cpGo = yieldNameAndContent dstDir .| saveContent
+  , cpGo = findPath d .| save
   , cpError = dumpParseException
   }
+  where
+    findPath
+      :: (Monad m)
+      => FilePath
+      -> ConduitM (ByteString, Product) (FilePath, ByteString) m ()
+    findPath dstDir = CC.map go where
+      go (content, p) = (dstDir </> productBasename p, content)
 
--- | Specializes 'pipeEitherDecoded' to yield 'Product'
-pipeProducts
-  :: (Monad m, MonadThrow m)
-  => ConduitM ByteString (Either ParseException Product) m ()
-pipeProducts = pipeEitherDecoded
+    save :: (Monad m, MonadResource m) => ConduitM (FilePath, ByteString) o m ()
+    save = awaitForever $ \(path, content) -> do
+      liftIO $ createDirectoryIfMissing True $ takeDirectory path
+      yield content .| CC.sinkFile path
 
--- | Specializes 'pipeEitherDecodedKeep' to yield 'Product'
+-- | Specializes 'pipeEitherDecodedKeep' for 'Product'
 pipeProductsKeep
   :: (Monad m, MonadThrow m)
   => ConduitM ByteString (Either ParseException (ByteString, Product)) m ()
 pipeProductsKeep = pipeEitherDecodedKeep
-
--- | Yields the filename and Yaml-encoded output of a 'Product'
-yieldNameAndContent
-  :: (Monad m)
-  => FilePath
-  -> ConduitM (ByteString, Product) (FilePath, ByteString) m ()
-yieldNameAndContent dstDir = CC.map go where
-  go (content, p) = (dstDir </> productBasename p, content)
-
--- | Yields the filename and Yaml-encoded output of a 'Product'
-saveContent
-  :: (Monad m, MonadResource m)
-  => ConduitM (FilePath, ByteString) o m ()
-saveContent = awaitForever $ \(path, content) -> do
-  liftIO $ createDirectoryIfMissing True $ takeDirectory path
-  yield content .| CC.sinkFile path
-
--- | To generate from the combined data
--- save the products
--- save the en descriptions
--- scan the product directory, report on files that do not have product description pair
--- forward pairs use the combined data-type to the html generator pipeline
-
 
 -- | Process the target YAML files in a srcDir into outputs in dstDir
 --
