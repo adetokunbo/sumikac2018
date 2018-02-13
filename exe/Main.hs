5c6fb223fd6b4d8088fa875134120ff537df36a9
Tim Emiola <adetokunbo@users.noreply.github.com>
2018-02-13 13:33:03 +0900

    Use main to drive the Yaml conversion process

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,5 +1,9 @@
 module Main where
 
+import Data.Sumikac.Conduit
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  let src = "/Users/tbetbetbe/tmp/test_download2/"
+  let dst = "/Users/tbetbetbe/tmp/remove_me"
+  runAll src dst
